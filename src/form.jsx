3bd8632de9de9ce5c94740508080b6e158e38157
𝐀𝐛𝐡𝐢𝐬𝐡𝐞𝐤 𝐏𝐚𝐭𝐞𝐥 <abhishekpatel4220@gmail.com>
2025-01-06 14:49:46 +0000

    using Usestate

---
+++ src/form.jsx
@@ -0,0 +1,36 @@
+import { useState } from "react";
+
+function Form({ onSubmit, onSort }) {
+  const [input, setInput] = useState("");
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (!input) return;
+
+    onSubmit(input);
+
+    setInput("");
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <fieldset>
+        <legend>Add a book</legend>
+        <input
+          type="text"
+          name="book"
+          placeholder="Add a book"
+          autoComplete="off"
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+        />
+        <button type="button" onClick={onSort}>
+          sort
+        </button>
+        <button type="submit">Add book</button>
+      </fieldset>
+    </form>
+  );
+}
+
+export default Form;
